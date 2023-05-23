095ddf3aabaace7efea1757efb5d812f86d90c4b
JaeWoo Joe <joejw7554@knights.ucf.edu>
2023-05-23 15:52:43 -0400

    Change1

---
+++ JWAxeGame/axe_game.cpp
@@ -0,0 +1,12 @@
+#include "raylib.h"
+
+int main()
+{
+    float root_beer = 1.99;
+    double cheese_burger = 5.99;
+    bool ShouldHaveLunch = true;
+
+    int width = 350;
+    int height = 200;
+    InitWindow(width, height, "JWGame");
+}
