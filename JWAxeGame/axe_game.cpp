a94338184aa0ff666df276e5639daf318d3d6e73
JaeWoo Joe <joejw7554@knights.ucf.edu>
2023-05-24 00:19:54 -0400

    Basic Movement

--- JWAxeGame/axe_game.cpp
+++ JWAxeGame/axe_game.cpp
@@ -2,11 +2,45 @@
 
 int main()
 {
-    float root_beer = 1.99;
-    double cheese_burger = 5.99;
-    bool ShouldHaveLunch = true;
 
-    int width = 350;
-    int height = 200;
+    int width{350};
+    int height{200};
+
+    // Circle Coordinates
+    int XCoordinate{width / 2};
+    int YCoordinate{height / 2};
+    int radius = 25;
+    int Speed = 10;
+
     InitWindow(width, height, "JWGame");
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() != true)
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Game logic Begins
+        DrawCircle(XCoordinate, YCoordinate, radius, BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            XCoordinate += Speed;
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            XCoordinate -= Speed;
+        }
+        if (IsKeyDown(KEY_W))
+        {
+            YCoordinate -= Speed;
+        }
+        if (IsKeyDown(KEY_S))
+        {
+            YCoordinate += Speed;
+        }
+
+        // Game logic Ends
+        EndDrawing();
+    }
 }
